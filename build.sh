1ca810980c437e28ae6a27e247a47441e4ecf620
Chanwit Kaewkasi <chanwit@gmail.com>
2011-04-21 21:29:41 +0700

    update ZK to 5.0.7-FL 2011-04-20

--- build.sh
+++ build.sh
@@ -1,2 +1,26 @@
-echo "build"
+ZK_VER=5.0.7-FL-2011-04-20
+ZKGRAILS_VER=1.1-M1
+
+cd zkgrails-aspect
+./build.sh $ZK_VER
+
+cd ..
+cd zkgrails
+./copylib ../zkgrails-aspect/injar/zkee-bin-eval-$ZK_VER
+grails clean && grails package-plugin
+
+cd ..
+cd zkgrails-ee
+./copylib ../zkgrails-aspect/injar/zkee-bin-eval-$ZK_VER
+grails clean && grails package-plugin
+
+cd ..
+cd zkgrails-hibernate
+grails install-plugin --non-interactive ../zkgrails/grails-zk-$ZKGRAILS_VER.zip
+grails clean && grails package-plugin
+
+cd ..
+cd zkgrails-codemirror
+grails install-plugin --non-interactive ../zkgrails/grails-zk-$ZKGRAILS_VER.zip
+grails clean && grails package-plugin
 
